295cba5b36fc92071f1d7bf59b49ef3ba2aba550
Esben Skov Pedersen <esp@skovpedeconsult.dk>
2019-01-08 23:05:00 +0100

    init

---
+++ LightServer/VeraController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LightServer
+{
+    public class VeraController : Controller
+    {
+        public static object Last { get; private set; }
+        private readonly IConfiguration configuration;
+        Regex regex = new Regex(@"^dev_(\d+)");
+
+        public VeraController(IConfiguration configuration) => this.configuration = configuration;
+        public static void LoadFile()
+        {
+            try
+            {
+                Last = System.IO.File.ReadAllText(AppDomain.CurrentDomain.GetData("DataDirectory").ToString() + "/latest.json");
+            }
+            catch (Exception) { }
+        }
+
+        [HttpPut("vera")]
+        public void Index([FromBody]VeraModel.RootObject model)
+        {
+            var l =
+                (from config in configuration.GetChildren()
+                 let match = regex.Match(config.Key)
+                 where match.Success
+                 from d in model.devices
+                 where d.id.ToString() == match.Groups[1].Value
+                 from s in d.states.Where(state => state.service == "urn:upnp-org:serviceId:SwitchPower1").Take(1)
+                 select new
+                 {
+                     d.id,
+                     @on = s.value == "1",
+                     name = config.Value
+                 }).ToList();
+            System.IO.File.WriteAllText(AppDomain.CurrentDomain.GetData("DataDirectory").ToString() + "/latest.json", JsonConvert.SerializeObject(l));
+            Last = l;
+        }
+    }
+}
---
+++ LightServer/VeraModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightServer
+{
+    public class VeraModel
+    {
+        public class RootObject
+        {
+            public Startup startup { get; set; }
+            public Device[] devices { get; set; }
+            public Scene[] scenes { get; set; }
+            public int Using_2G { get; set; }
+            public int LoadTime { get; set; }
+            public int DataVersion { get; set; }
+            public int UserData_DataVersion { get; set; }
+            public int TimeStamp { get; set; }
+            public int lights_on { get; set; }
+            public int lights_off { get; set; }
+            public int doors_locked { get; set; }
+            public int doors_unlocked { get; set; }
+            public int sensors_tripped { get; set; }
+            public int sensors_not_tripped { get; set; }
+            public int failed_devices { get; set; }
+            public int visible_devices { get; set; }
+            public int partitions_active { get; set; }
+            public int partitions_notactive { get; set; }
+            public Alert[] alerts { get; set; }
+            public int ZWaveStatus { get; set; }
+            public int Mode { get; set; }
+            public string LocalTime { get; set; }
+        }
+
+        public class Startup
+        {
+            public Task[] tasks { get; set; }
+        }
+
+        public class Task
+        {
+            public int id { get; set; }
+            public int status { get; set; }
+            public string type { get; set; }
+            public string comments { get; set; }
+        }
+
+        public class Device
+        {
+            public int id { get; set; }
+            public State[] states { get; set; }
+            public object[] Jobs { get; set; }
+            public int PendingJobs { get; set; }
+            public Tooltip tooltip { get; set; }
+            public int status { get; set; }
+        }
+
+        public class Tooltip
+        {
+            public int display { get; set; }
+            public string tag2 { get; set; }
+        }
+
+        public class State
+        {
+            public int id { get; set; }
+            public string service { get; set; }
+            public string variable { get; set; }
+            public string value { get; set; }
+        }
+
+        public class Scene
+        {
+            public int id { get; set; }
+            public object[] Jobs { get; set; }
+            public Tooltip1 tooltip { get; set; }
+            public int status { get; set; }
+            public bool active { get; set; }
+        }
+
+        public class Tooltip1
+        {
+            public int display { get; set; }
+        }
+
+        public class Alert
+        {
+            public string PK_Alert { get; set; }
+            public string LocalDate { get; set; }
+            public int EventType { get; set; }
+            public int SourceType { get; set; }
+            public int Argument { get; set; }
+            public int Filesize { get; set; }
+            public int Severity { get; set; }
+            public int LocalTimestamp { get; set; }
+            public string Code { get; set; }
+            public string NewValue { get; set; }
+            public string Description { get; set; }
+            public string Users { get; set; }
+            public string Server_Storage { get; set; }
+            public string Key { get; set; }
+            public string Icon { get; set; }
+            public string PK_Store { get; set; }
+        }
+
+    }
+}
