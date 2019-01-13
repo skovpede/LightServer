375b10797df187ea514e456db03e116447e13177
Esben Skov Pedersen <esp@skovpedeconsult.dk>
2019-01-13 21:40:05 +0100

    expose last input

--- LightServer/VeraController.cs
+++ LightServer/VeraController.cs
@@ -13,6 +13,8 @@ namespace LightServer
     public class VeraController : Controller
     {
         public static object Last { get; private set; }
+        private static VeraModel.RootObject LastInput;
+
         private readonly IConfiguration configuration;
         Regex regex = new Regex(@"^dev_(\d+)");
 
@@ -29,21 +31,25 @@ namespace LightServer
         [HttpPut("vera")]
         public void Index([FromBody]VeraModel.RootObject model)
         {
-            var l =
-                (from config in configuration.GetChildren()
-                 let match = regex.Match(config.Key)
-                 where match.Success
-                 from d in model.devices
-                 where d.id.ToString() == match.Groups[1].Value
-                 from s in d.states.Where(state => state.service == "urn:upnp-org:serviceId:SwitchPower1").Take(1)
-                 select new
-                 {
-                     d.id,
-                     @on = s.value == "1",
-                     name = config.Value
-                 }).ToList();
-            //System.IO.File.WriteAllText(AppDomain.CurrentDomain.GetData("DataDirectory").ToString() + "/latest.json", JsonConvert.SerializeObject(l));
-            Last = l;
+            LastInput = model;
+            //var l =
+            //    (from config in configuration.GetChildren()
+            //     let match = regex.Match(config.Key)
+            //     where match.Success
+            //     from d in model.devices
+            //     where d.id.ToString() == match.Groups[1].Value
+            //     from s in d.states.Where(state => state.service == "urn:upnp-org:serviceId:SwitchPower1").Take(1)
+            //     select new
+            //     {
+            //         d.id,
+            //         @on = s.value == "1",
+            //         name = config.Value
+            //     }).ToList();
+            ////System.IO.File.WriteAllText(AppDomain.CurrentDomain.GetData("DataDirectory").ToString() + "/latest.json", JsonConvert.SerializeObject(l));
+            //Last = l;
         }
+
+        [HttpGet("vera")]
+        public object Get() => LastInput;
     }
 }
